a48b5047c59d3e8d09777a7f4416d02af373ffc2
Camtriggerhappy <ckiw23@gmail.com>
2020-05-12 19:45:38 -0400

    Fixed

--- geometry/VolumeOfCone.py
+++ geometry/VolumeOfCone.py
@@ -1,13 +1,13 @@
 import math
 
 def VolumeOfCone(Pi,Height,Radius):
-    Volume = 12*(5**2)*(1/3)
+    Volume = (1/3)*(Height*(Radius**2)*(Pi))
     return Volume
 
 
-Pi = float(input("What are you using for PI"))
-Height = float(input("What are you using for the Height"))
-Radius = float(input("What are you using for the Radius"))
+Pi = float(input("What are you using for PI "))
+Height = float(input("What are you using for the Height "))
+Radius = float(input("What are you using for the Radius "))
 
 
 print(VolumeOfCone(Pi,Height,Radius))
